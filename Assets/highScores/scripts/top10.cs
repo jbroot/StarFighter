18e0bbb2d25ed07f79aa8c7950827a2a1c5802e1
James Larson <jameslarson4@mail.weber.edu>
2018-11-01 16:11:04 -0600

    high scores web request code

--- Assets/highScores/scripts/top10.cs
+++ Assets/highScores/scripts/top10.cs
@@ -1,27 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class top10 : MonoBehaviour {
-
-    string[] top10Array = new string[10]{"Zeus", "Muhammad", "Washington", "Jo", "Jane", "Jax", "noob123",
-        "Orange Man", "Cheater", "King Kong" };
-    int[] scores = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+public class top10 : MonoBehaviour
+{
     public Text namesBox;
-    public Text scoresBox;
+    public string URL = "http://spacefighterweb.azurewebsites.net/api/values";
+    public Text responseText;
 
 	// Use this for initialization
-	void Start () {
-        namesBox.text = scoresBox.text = "";
-        for (int i = 0; i < 10; i++)
-        {
-            namesBox.text += (i + 1).ToString() + ". " + top10Array[i] + "\n";
-            scoresBox.text += scores[i].ToString() + "\n";
-        }
-
+	void Start()
+    {
+        namesBox.text = "";
+        Request();
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Request()
+    {
+        WWW request = new WWW(URL);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+        responseText.text = req.text;
+        namesBox.text = req.text;
+    }
 }
