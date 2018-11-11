85999bf86e3ea2371ab6370bc6dd56d73e762bda
Margaret <midgeschroeder@gmail.com>
2018-11-11 12:44:50 -0700

    Change the location of the sprite sheet. Create a folder for backgrounds and other images. These are under Universal. Create a new folder under asset called ApplicationScenes. Moved existing GamePlay and High Scores folders uner this. The scripts for each scene appear to be in each of the subfolders for the game but also created a folder for Universal scripts.

---
+++ Assets/ApplicationScenes/gamePlay/scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float maxSpeed = 5f;
+    public float rotationSpeed = 180f;
+	
+	// Update is called once per frame
+	void Update () {
+        // Get the Quaternion
+        Quaternion rot = transform.rotation;
+
+        //Get Z Euler Angles
+        float z = rot.eulerAngles.z;
+
+        //Change Z angle based on input
+        z -= Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+        //Recreate the Quaternion
+        rot = Quaternion.Euler(0, 0, z);
+
+        //Feed Quaternion into our rotation
+        transform.rotation = rot;
+
+        //Move the ship
+        Vector3 pos = transform.position;
+
+        Vector3 vel = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime,0);
+
+
+        pos += rot * vel;
+
+        transform.position = pos;
+
+
+
+
+
+
+	}
+}
