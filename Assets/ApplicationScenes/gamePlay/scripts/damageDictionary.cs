13c5d3dcd6fd0ebc7a99b630108676fa8b2e7c4c
unknown <jarren.briscoe@gmail.com>
2018-11-16 13:36:04 -0700

    added Damage Dictionary

---
+++ Assets/ApplicationScenes/gamePlay/scripts/damageDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageDictionary : MonoBehaviour {
+
+    public Dictionary<string, double> damages = new Dictionary<string, double>();
+
+	// Use this for initialization
+	void Start () {
+        damages.Add("baseBotContact", 20);
+        damages.Add("droneContact", 30);
+        damages.Add("playerContact", 20);
+        damages.Add("spawnerContact", 40);
+        damages.Add("baseLaser", 10);
+	}
+}
