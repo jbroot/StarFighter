c767fe62fe4d4616f09317a63ce94737ce6603e2
Margaret <midgeschroeder@gmail.com>
2018-11-18 00:10:04 -0700

    Create a new object score to hold both the username and score. Redo the high scores page by creating specific functions to handle the sorting and displaying of data.

--- Assets/ApplicationScenes/highScores/scripts/top10.cs
+++ Assets/ApplicationScenes/highScores/scripts/top10.cs
@@ -1,27 +1,61 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;  
 
 public class top10 : MonoBehaviour {
 
-    string[] top10Array = new string[10]{"Zeus", "Muhammad", "Washington", "Jo", "Jane", "Jax", "noob123",
-        "Orange Man", "Cheater", "King Kong" };
-    int[] scores = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    public Text namesBox;
-    public Text scoresBox;
+    private List<Score> scoresInOrder = new List<Score>();
 
 	// Use this for initialization
-	void Start () {
-        namesBox.text = scoresBox.text = "";
+    public void Start() {
+        AddDummyDataToHighScores(); //TODO: Replace with data retrieved from the database
+        SortScoresHighestToLowest();
+        DisplayTopTenHighestScoresNames();
+        DisplayTopTenHighestScores();
+    }
+
+    public void DisplayTopTenHighestScores(){
+        Text scoresBox = GameObject.Find("scoresBox").GetComponent<Text>();
+        scoresBox.text = "";
         for (int i = 0; i < 10; i++)
         {
-            namesBox.text += (i + 1).ToString() + ". " + top10Array[i] + "\n";
-            scoresBox.text += scores[i].ToString() + "\n";
+            Score s = (Score)scoresInOrder[i];
+            int score = s.TotalPoints;
+            scoresBox.text += score.ToString() + "\n";
+        }
+    }
+
+    public void DisplayTopTenHighestScoresNames()
+    {
+        Text namesBox = GameObject.Find("namesBox").GetComponent<Text>();
+        namesBox.text = "";
+        for (int i = 0; i < 10; i++)
+        {
+            Score s = (Score)scoresInOrder[i];
+            string username = s.Username;
+            namesBox.text += username + "\n";
+        }
+    }
+
+    private void AddDummyDataToHighScores(){
+        string[] names = new string[10]{"Zeus", "Muhammad", "Washington", "Jo", "Jane", "Jax", "noob123",
+        "Orange Man", "Cheater", "King Kong" };
+        int[] scores = new int[10] { 1000, 30000, 500, 70, 8000, 600000, 0, 7770, 990, 10 };
+
+        for (int i = 0; i < 10; i++){
+            Score s = new Score(names[i], scores[i]);
+            scoresInOrder.Add(s);
         }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void SortScoresHighestToLowest(){
+        scoresInOrder.Sort();
+        scoresInOrder.Reverse();
+    }
+
+    public void AddNewScore(string username, int score){
+        Score s = new Score(username, score);
+        scoresInOrder.Add(s);
+    } 
 }
