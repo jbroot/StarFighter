d31247db9a4ab7128df10237a049a692d3a3de5e
Margaret <midgeschroeder@gmail.com>
2018-11-29 21:36:24 -0700

    Add ship change sound effects and re-add the ship change functionality on main menu

--- Assets/ApplicationScenes/MainMenu/Scripts/ShipMenuOptions.cs
+++ Assets/ApplicationScenes/MainMenu/Scripts/ShipMenuOptions.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ShipMenuOptions : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //default red fighter ship on game start
+    public string selectedShipColor = "red";
+    MainMenu mm = new MainMenu();
+
+    public void ChangeSelectedShipColor(string shipColor)
+    {
+        selectedShipColor = shipColor;
+        mm.UpdateSelectedShipColor(selectedShipColor);
+    }
+
+    public string GetSelectedShipColor()
+    {
+        return selectedShipColor;
+    }
+
+
 }
