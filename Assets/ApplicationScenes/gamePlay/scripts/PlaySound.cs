ababaeca894053e9ee006f55b4ddf7b821dba0e3
camcom421 <cameron.winkley@gmail.com>
2018-12-04 20:59:20 -0700

    added pick up item

---
+++ Assets/ApplicationScenes/gamePlay/scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter()
+	{
+		AudioSource source = GetComponent<AudioSource>();
+		source.Play();
+	}
+}
